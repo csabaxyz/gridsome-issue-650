6f47dc90d9fe55c150d0b14ea4ccf32167e1daf9
Csaba Szabo <34774629+csabaxyz@users.noreply.github.com>
2020-06-14 19:17:19 +0200

    Add sample repro code for https://github.com/gridsome/gridsome/issues/650

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>About page</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About'
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,36 @@
 <template>
   <Layout>
+    <g-link to="/about">About</g-link>
+    <ul>
+      <li v-for="item in list" :key="item.id">{{ item.title }}</li>
+    </ul>
   </Layout>
 </template>
+
+<static-query>
+query {
+  metadata {
+    buildTime
+  }
+}
+</static-query>
 
 <script>
 export default {
   computed: {
+    listData() {
+      return [
+        { id: 1, title: 'Item 1', date: new Date('2020-01-01 00:00:00') },
+        { id: 2, title: 'Item 2', date: new Date('2020-01-05 00:00:00') },
+        // Set it to expire 2 minutes after the build
+        // This is to simulate what would happen if an item is added at a later date to the list
+        { id: 3, title: 'Item 3', date: new Date(Number(this.$static.metadata.buildTime) + 2 * 60 * 1000) },
+      ]
+    },
+
+    list() {
+      return this.listData.filter(d => d.date < Date.now())
+    }
   },
 
   metaInfo: {
